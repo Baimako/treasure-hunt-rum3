49d439b65ea87f04e9febdabf26856a21369c8eb
Ashish Aryal <ashish.aryal@chasacademy.se>
2025-09-25 11:17:51 +0200

    added click and tressure counter

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 // track any clicks to
+
+var clickCounter = 0
+var tressuresFound = 0
 document.addEventListener('click', function(event) {
     let idOfElement = event.target;
-    if (!idOfElement.id) {
-        console.log("id is empty")
-    } else {
+
+    if (!idOfElement.id ||
+        !idOfElement.id.startsWith("tressure"))
+    {
+        console.log("id mismatch");
+    } else
+    {
         idOfElement.classList.add('found');
+        tressuresFound += 1;
+        idOfElement.removeAttribute('id');
     }
+    clickCounter += 1;
+
+    console.log("tressures found: " + tressuresFound);
 });
+
 
 
