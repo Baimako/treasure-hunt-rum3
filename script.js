b2dd6794a7e3b5291f34ee4ebbeeb97d5be63632
Ashish Aryal <ashish.aryal@chasacademy.se>
2025-09-25 12:00:52 +0200

    script finalised

--- script.js
+++ script.js
@@ -1,38 +1,40 @@
-
+// initialise the variables necessary
 var clickCounter = 0
 var tressuresFound = 0
 var totalSeconds = 0;
 
-
+// get all the interactive items
 const timerDisplay =  document.getElementById("timer")
 const timerInterval = setInterval(updateTimer, 1000);
+const tressureFoundCounter  = document.getElementById("foundCount")
+const statement = document.getElementById("statement")
 
+
+// track all the clicks
 document.addEventListener('click', function(event) {
     let idOfElement = event.target;
 
     if (!idOfElement.id ||
-        !idOfElement.id.startsWith("tressure"))
-    {
+        !idOfElement.id.startsWith("treasure")) {
         console.log("id mismatch");
-    } else
-    {
+    } else {
         idOfElement.classList.add('found');
         tressuresFound += 1;
         idOfElement.removeAttribute('id');
     }
     clickCounter += 1;
 
-    if (tressuresFound >= 10)
-    {
+    if (tressuresFound >= 12) {
         clearInterval(timerInterval);
+        statement.innerText = "You found all tressures in " + totalSeconds + " seconds and it took total of " + clickCounter + " clicks";
+
     }
-    console.log("tressures found: " + tressuresFound);
-    console.log("total seconds: " + totalSeconds);
-});
+
+    tressureFoundCounter.innerText = tressuresFound;
+
+    })
 
 
-
-//
 function updateTimer() {
     totalSeconds++;
 
